12eebad111794c22a32a8a79097eec4498ebd782
Jon Mease <jon.mease.dev@gmail.com>
2021-12-03 07:51:21 -0500

    skeleton pyo3 module setup

--- vegafusion-python/src/lib.rs
+++ vegafusion-python/src/lib.rs
@@ -1,3 +1,40 @@
+use pyo3::prelude::*;
+
+
+#[pyclass]
+struct PySpecPlan {
+    #[pyo3(get)]
+    full_spec: String,
+
+    #[pyo3(get)]
+    server_spec: String,
+
+    #[pyo3(get)]
+    client_spec: String,
+}
+
+#[pymethods]
+impl PySpecPlan {
+    #[new]
+    fn new(full_spec: String, server_spec: String, client_spec: String) -> Self {
+        Self {
+            full_spec, server_spec, client_spec
+        }
+    }
+}
+
+
+
+/// A Python module implemented in Rust. The name of this function must match
+/// the `lib.name` setting in the `Cargo.toml`, else Python will not be able to
+/// import the module.
+#[pymodule]
+fn vegafusion(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_class::<PySpecPlan>()?;
+    Ok(())
+}
+
+
 #[cfg(test)]
 mod tests {
     #[test]
