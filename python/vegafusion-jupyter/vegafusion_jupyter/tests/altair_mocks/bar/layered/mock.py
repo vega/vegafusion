b3fd36536b2ccce3adb786472bb5e623cc8d5895
Jon Mease <jon.mease.dev@gmail.com>
2021-12-19 05:16:25 -0500

    Update bar layered test to use explicit time encoding

--- python/vegafusion-jupyter/vegafusion_jupyter/tests/altair_mocks/bar/layered/mock.py
+++ python/vegafusion-jupyter/vegafusion_jupyter/tests/altair_mocks/bar/layered/mock.py
@@ -1,12 +1,12 @@
 # https://altair-viz.github.io/gallery/layered_bar_chart.html
-
+# With explicit temporal encoding
 import altair as alt
 from vega_datasets import data
 
 source = data.iowa_electricity()
 
 alt.Chart(source).mark_bar(opacity=0.7).encode(
-    x='year:O',
+    x='year:T',
     y=alt.Y('net_generation:Q', stack=None),
     color="source",
 )
