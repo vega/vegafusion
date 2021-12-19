6f2275baeb1bb7f754852b7bc4df3795f280eae5
Jon Mease <jon.mease.dev@gmail.com>
2021-12-19 05:15:28 -0500

    Fix trail marker test by
    Converting the year column to a string. VegaFusion doesn't need this, but the altair case isn't handling
    an integer year column correctly.

--- python/vegafusion-jupyter/vegafusion_jupyter/tests/altair_mocks/line/trail_marker/mock.py
+++ python/vegafusion-jupyter/vegafusion_jupyter/tests/altair_mocks/line/trail_marker/mock.py
@@ -1,9 +1,12 @@
 # https://altair-viz.github.io/gallery/trail_marker.html
+# With year column converted to a string. VegaFusion doesn't need this, but the altair case isn't handling
+# an integer year column correctly.
 
 import altair as alt
 from vega_datasets import data
 
 source = data.wheat()
+source["year"] = source.year.astype(str)
 
 alt.Chart(source).mark_trail().encode(
     x='year:T',
