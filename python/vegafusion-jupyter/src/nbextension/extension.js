74a1ed1ebc1fda2232122e3b44f0081af1a79378
Jon Mease <jon.mease.dev@gmail.com>
2022-01-17 06:28:36 -0500

    Add extension.js that gets copied to nbextension directory

---
+++ python/vegafusion-jupyter/src/nbextension/extension.js
@@ -0,0 +1,36 @@
+/*
+ * VegaFusion
+ * Copyright (C) 2022 Jon Mease
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public
+ * License along with this program.
+ * If not, see http://www.gnu.org/licenses/.
+ */
+
+// Entry point for the notebook bundle containing custom model definitions.
+//
+define(function() {
+    "use strict";
+
+    window['requirejs'].config({
+        map: {
+            '*': {
+                'vegafusion-jupyter': 'nbextensions/vegafusion-jupyter/index',
+            },
+        }
+    });
+    // Export the required load_ipython_extension function
+    return {
+        load_ipython_extension : function() {}
+    };
+});
