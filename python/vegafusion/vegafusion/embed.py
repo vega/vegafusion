e0b907aaefdf2d7627b0343d1c1555a80720136e
Jon Mease <jon.mease.dev@gmail.com>
2022-02-05 06:41:55 -0500

    Fix name of vegafusion-python-embed package

--- python/vegafusion/vegafusion/embed.py
+++ python/vegafusion/vegafusion/embed.py
@@ -16,4 +16,4 @@
 try:
     from vegafusion_embed import *
 except ImportError:
-    raise ImportError("Please install the vegafusion-embed package")
+    raise ImportError("Please install the vegafusion-python-embed package")
