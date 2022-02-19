4eff0982fe81519b8ddf3f1b4f4199b9520cc436
Jon Mease <jon.mease.dev@gmail.com>
2022-02-19 08:27:30 -0500

    Add initial vegafusion-server application crate

---
+++ vegafusion-server/src/vegafusion-server.rs
@@ -0,0 +1,69 @@
+use tonic::{transport::Server, Request, Response, Status, Code};
+use vegafusion_core::proto::gen::services::{QueryRequest, QueryResult};
+use vegafusion_core::proto::gen::services::vega_fusion_runtime_server::{
+    VegaFusionRuntime as TonicVegaFusionRuntime,
+    VegaFusionRuntimeServer as TonicVegaFusionRuntimeServer,
+};
+
+use vegafusion_rt_datafusion::task_graph::runtime::TaskGraphRuntime;
+
+#[derive(Clone)]
+pub struct VegaFusionRuntimeGrpc {
+    runtime: TaskGraphRuntime
+}
+
+impl VegaFusionRuntimeGrpc {
+    pub fn new(capacity: Option<usize>, memory_limit: Option<usize>) -> VegaFusionRuntimeGrpc {
+        VegaFusionRuntimeGrpc {
+            runtime: TaskGraphRuntime::new(capacity, memory_limit)
+        }
+    }
+}
+
+#[tonic::async_trait]
+impl TonicVegaFusionRuntime for VegaFusionRuntimeGrpc  {
+    async fn task_graph_query(&self, request: Request<QueryRequest>) -> Result<Response<QueryResult>, Status> {
+        println!("Request: {:#?}", request);
+        let result = self.runtime.query_request(request.into_inner()).await;
+        match result {
+            Ok(result) => {
+                Ok(Response::new(result))
+            }
+            Err(err) => {
+                Err(Status::unknown(err.to_string()))
+            }
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "[::1]:50051".parse()?;
+
+    // Get from command line
+    let capacity: Option<usize> = Some(128);
+    let memory_limit: Option<usize> = Some(2e9 as usize);
+    let server = VegaFusionRuntimeGrpc::new(capacity, memory_limit);
+    let server = TonicVegaFusionRuntimeServer::new(server);
+
+    let grpc_web = true;
+    if grpc_web {
+        let server = tonic_web::config()
+            // .allow_origins(vec!["127.0.0.1"])
+            .allow_all_origins()
+            .enable(server);
+
+        Server::builder()
+            .accept_http1(true)
+            .add_service(server)
+            .serve(addr)
+            .await?;
+    } else {
+        Server::builder()
+            .add_service(server)
+            .serve(addr)
+            .await?;
+    };
+
+    Ok(())
+}
---
+++ vegafusion-server/tests/test_task_graph_runtime.rs
@@ -0,0 +1,121 @@
+/*
+ * VegaFusion
+ * Copyright (C) 2022 Jon Mease
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License along with this program.
+ * If not, see http://www.gnu.org/licenses/.
+ */
+// use std::sync::Arc;
+// use vegafusion_core::data::scalar::ScalarValue;
+// use vegafusion_core::expression::parser::parse;
+// use vegafusion_core::proto::gen::tasks::data_url_task::Url;
+// use vegafusion_core::proto::gen::tasks::{
+//     DataSourceTask, DataUrlTask, NodeValueIndex, Task, TaskGraph, Variable,
+// };
+// use vegafusion_core::proto::gen::transforms::transform::TransformKind;
+// use vegafusion_core::proto::gen::transforms::{
+//     Collect, Extent, SortOrder, Transform, TransformPipeline,
+// };
+// use vegafusion_core::spec::chart::ChartSpec;
+// use vegafusion_core::task_graph::scope::TaskScope;
+// use vegafusion_core::task_graph::task_value::TaskValue;
+// use vegafusion_rt_datafusion::task_graph::runtime::TaskGraphRuntime;
+//
+
+use std::sync::Arc;
+use serde_json::json;
+use vegafusion_core::data::scalar::{ScalarValue, ScalarValueHelpers};
+use vegafusion_core::proto::gen::services::{query_request, QueryRequest, QueryResult};
+use vegafusion_core::proto::gen::services::query_result::Response;
+use vegafusion_core::proto::gen::tasks::{NodeValueIndex, TaskGraph, TaskGraphValueRequest, VariableNamespace};
+use vegafusion_core::spec::chart::ChartSpec;
+
+use vegafusion_core::proto::gen::services::vega_fusion_runtime_client::VegaFusionRuntimeClient;
+use vegafusion_core::task_graph::task_value::TaskValue;
+
+#[tokio::test(flavor = "multi_thread")]
+async fn try_it_from_spec() {
+    let chart: ChartSpec = serde_json::from_str(
+        r##"{
+  "signals": [
+    {
+      "name": "url",
+      "value": "https://raw.githubusercontent.com/vega/vega-datasets/master/data/penguins.json"
+    }
+  ],
+  "data": [
+    {
+      "name": "url_datasetA",
+      "url": {"signal": "url"}
+    },
+    {
+      "name": "datasetA",
+      "source": "url_datasetA",
+      "transform": [
+        {
+          "type": "extent",
+          "signal": "my_extent",
+          "field": "Beak Length (mm)"
+        },
+        {
+          "type": "collect",
+          "sort": {"field": "Beak Length (mm)"}
+        }
+      ]
+    }
+  ]
+}
+"##,
+    )
+    .unwrap();
+
+    let task_scope = chart.to_task_scope().unwrap();
+    let tasks = chart.to_tasks().unwrap();
+
+    let graph = TaskGraph::new(tasks, &task_scope).unwrap();
+    let request = QueryRequest { request: Some(query_request::Request::TaskGraphValues(
+        TaskGraphValueRequest {
+            task_graph: Some(graph),
+            indices: vec![
+                NodeValueIndex::new(2, Some(0)),
+            ]
+        }
+    )) };
+
+    let mut client = VegaFusionRuntimeClient::connect("http://[::1]:50051").await.expect(
+        "Failed to connect to gRPC server"
+    );
+    let response = client.task_graph_query(request).await.unwrap();
+
+    let query_result = response.into_inner();
+    match query_result.response.unwrap() {
+        Response::Error(error) => {
+            panic!("Error: {:?}", error)
+        }
+        Response::TaskGraphValues(values_response) => {
+            let response_values = values_response.deserialize().unwrap();
+            println!("Result: {:#?}", response_values);
+            assert_eq!(response_values.len(), 1);
+            let (var, scope, value) = &response_values[0];
+
+            assert_eq!(var.name.as_str(), "my_extent");
+            assert_eq!(var.namespace(), VariableNamespace::Signal);
+            assert_eq!(scope, &Vec::<u32>::new());
+            assert_eq!(
+                &value.as_scalar().unwrap().to_f64x2().unwrap(),
+                &[32.1, 59.6],
+            )
+        }
+    }
+}
