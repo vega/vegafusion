3e28b2b24b6c921921541e7a62d8261f65048343
Jon Mease <jon.mease.dev@gmail.com>
2022-04-22 08:03:44 -0400

    Cleanup copyright notice in file headers to refer to multiple licenses

--- vegafusion-rt-datafusion/src/expression/compiler/builtin_functions/array/mod.rs
+++ vegafusion-rt-datafusion/src/expression/compiler/builtin_functions/array/mod.rs
@@ -1,3 +1,11 @@
+/*
+ * VegaFusion
+ * Copyright (C) 2022 VegaFusion Technologies LLC
+ *
+ * This program is distributed under multiple licenses.
+ * Please consult the license documentation provided alongside
+ * this program the details of the active license.
+ */
 /*!
 ## Array Functions
 Functions for working with arrays of values.
--- vegafusion-rt-datafusion/src/expression/compiler/builtin_functions/control_flow/mod.rs
+++ vegafusion-rt-datafusion/src/expression/compiler/builtin_functions/control_flow/mod.rs
@@ -1,3 +1,11 @@
+/*
+ * VegaFusion
+ * Copyright (C) 2022 VegaFusion Technologies LLC
+ *
+ * This program is distributed under multiple licenses.
+ * Please consult the license documentation provided alongside
+ * this program the details of the active license.
+ */
 /*!
 ## Control Flow Functions
 See https://vega.github.io/vega/docs/expressions/#control-flow-functions
