c81e19da9377efa01df3f29ac31f5e38c882b268
Jon Mease <jon.mease.dev@gmail.com>
2022-06-14 16:17:15 -0400

    Re-enable build.rs

--- vegafusion-core/build.rs
+++ vegafusion-core/build.rs
@@ -9,28 +9,28 @@
 extern crate prost_build;
 
 fn main() {
-    // let mut prost_config = prost_build::Config::new();
-    // let outdir = concat!(env!("CARGO_MANIFEST_DIR"), "/src/proto/prost_gen");
-    // println!("outdir: {}", outdir);
-    // let prost_config = prost_config.out_dir(outdir);
-    //
-    // prost_config
-    //     .protoc_arg("--experimental_allow_proto3_optional")
-    //     .compile_protos(
-    //         &[
-    //             "src/proto/expression.proto",
-    //             "src/proto/transforms.proto",
-    //             "src/proto/tasks.proto",
-    //             "src/proto/errors.proto",
-    //             "src/proto/pretransform.proto",
-    //             "src/proto/services.proto",
-    //         ],
-    //         &["src/proto"],
-    //     )
-    //     .unwrap();
-    //
-    // #[cfg(feature = "tonic_support")]
-    // gen_tonic()
+    let mut prost_config = prost_build::Config::new();
+    let outdir = concat!(env!("CARGO_MANIFEST_DIR"), "/src/proto/prost_gen");
+    println!("outdir: {}", outdir);
+    let prost_config = prost_config.out_dir(outdir);
+
+    prost_config
+        .protoc_arg("--experimental_allow_proto3_optional")
+        .compile_protos(
+            &[
+                "src/proto/expression.proto",
+                "src/proto/transforms.proto",
+                "src/proto/tasks.proto",
+                "src/proto/errors.proto",
+                "src/proto/pretransform.proto",
+                "src/proto/services.proto",
+            ],
+            &["src/proto"],
+        )
+        .unwrap();
+
+    #[cfg(feature = "tonic_support")]
+    gen_tonic()
 }
 
 #[cfg(feature = "tonic_support")]
