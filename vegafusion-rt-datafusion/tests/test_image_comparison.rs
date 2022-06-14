3e8d48eec904e448b408a6b321a042fab7232fec
Jon Mease <jon.mease.dev@gmail.com>
2022-06-14 16:00:55 -0400

    Fix test name

--- vegafusion-rt-datafusion/tests/test_image_comparison.rs
+++ vegafusion-rt-datafusion/tests/test_image_comparison.rs
@@ -117,7 +117,7 @@ mod test_custom_specs {
         case("custom/movies_agg_parameterize", 0.001),
         case("custom/escaped_column_name1", 0.001),
         case("custom/layered_movies", 0.001),
-        case("custom/layered_movies", 0.001)
+        case("custom/shipping_mixed_scales", 0.001)
     )]
     fn test_image_comparison(spec_name: &str, tolerance: f64) {
         println!("spec_name: {}", spec_name);
