fd2afb368fb266d9c82c718edda03857bb640a34
Jon Mease <jon.mease.dev@gmail.com>
2022-07-16 07:57:17 -0500

    Use parameterize stack tests by offset

--- vegafusion-rt-datafusion/tests/test_transform_stack.rs
+++ vegafusion-rt-datafusion/tests/test_transform_stack.rs
@@ -21,146 +21,203 @@ use vegafusion_core::spec::transform::TransformSpec;
 use vegafusion_core::spec::values::{
     CompareSpec, Field, SortOrderOrList, SortOrderSpec, StringOrStringList,
 };
+use rstest::rstest;
 
-#[test]
-fn test_stack() {
-    let dataset = vega_json_dataset("penguins");
 
-    let stack_spec = StackTransformSpec {
-        field: Field::String("Body Mass (g)".to_string()),
-        groupby: None,
-        sort: None,
-        as_: None,
-        offset: None,
-        extra: Default::default(),
-    };
+#[cfg(test)]
+mod test_stack {
+    use vegafusion_core::spec::transform::stack::StackOffsetSpec;
+    use super::*;
 
-    let transform_specs = vec![TransformSpec::Stack(stack_spec)];
+    #[rstest(
+        offset,
+        case(None),
+        case(Some(StackOffsetSpec::Zero))
+    )]
+    fn test(offset: Option<StackOffsetSpec>) {
+        let dataset = vega_json_dataset("penguins");
 
-    let comp_config = Default::default();
-    let eq_config = TablesEqualConfig {
-        row_order: true,
-        ..Default::default()
-    };
+        let stack_spec = StackTransformSpec {
+            field: Field::String("Body Mass (g)".to_string()),
+            groupby: None,
+            sort: None,
+            as_: None,
+            offset,
+            extra: Default::default(),
+        };
 
-    check_transform_evaluation(
-        &dataset,
-        transform_specs.as_slice(),
-        &comp_config,
-        &eq_config,
-    );
+        let transform_specs = vec![TransformSpec::Stack(stack_spec)];
+
+        let comp_config = Default::default();
+        let eq_config = TablesEqualConfig {
+            row_order: true,
+            ..Default::default()
+        };
+
+        check_transform_evaluation(
+            &dataset,
+            transform_specs.as_slice(),
+            &comp_config,
+            &eq_config,
+        );
+    }
+
+    #[test]
+    fn test_marker() {} // Help IDE detect test module
 }
 
-#[test]
-fn test_stack_with_group() {
-    let dataset = vega_json_dataset("penguins");
 
-    let stack_spec = StackTransformSpec {
-        field: Field::String("Body Mass (g)".to_string()),
-        groupby: Some(vec![Field::String("Species".to_string())]),
-        sort: None,
-        as_: None,
-        offset: None,
-        extra: Default::default(),
-    };
+#[cfg(test)]
+mod test_stack_with_group {
+    use vegafusion_core::spec::transform::stack::StackOffsetSpec;
+    use super::*;
 
-    let transform_specs = vec![TransformSpec::Stack(stack_spec)];
+    #[rstest(
+        offset,
+        case(None),
+        case(Some(StackOffsetSpec::Zero))
+    )]
+    fn test(offset: Option<StackOffsetSpec>) {
+        let dataset = vega_json_dataset("penguins");
 
-    let comp_config = Default::default();
-    let eq_config = TablesEqualConfig {
-        row_order: true,
-        ..Default::default()
-    };
+        let stack_spec = StackTransformSpec {
+            field: Field::String("Body Mass (g)".to_string()),
+            groupby: Some(vec![Field::String("Species".to_string())]),
+            sort: None,
+            as_: None,
+            offset,
+            extra: Default::default(),
+        };
 
-    check_transform_evaluation(
-        &dataset,
-        transform_specs.as_slice(),
-        &comp_config,
-        &eq_config,
-    );
+        let transform_specs = vec![TransformSpec::Stack(stack_spec)];
+
+        let comp_config = Default::default();
+        let eq_config = TablesEqualConfig {
+            row_order: true,
+            ..Default::default()
+        };
+
+        check_transform_evaluation(
+            &dataset,
+            transform_specs.as_slice(),
+            &comp_config,
+            &eq_config,
+        );
+    }
+
+    #[test]
+    fn test_marker() {} // Help IDE detect test module
 }
 
-#[test]
-fn test_stack_with_group_sort() {
-    let dataset = vega_json_dataset("penguins");
 
-    let stack_spec = StackTransformSpec {
-        field: Field::String("Body Mass (g)".to_string()),
-        groupby: Some(vec![Field::String("Species".to_string())]),
-        sort: Some(CompareSpec {
-            field: StringOrStringList::StringList(vec![
-                "Sex".to_string(),
-                "Flipper Length (mm)".to_string(),
-            ]),
-            order: Some(SortOrderOrList::SortOrderList(vec![
-                SortOrderSpec::Ascending,
-                SortOrderSpec::Descending,
-            ])),
-        }),
-        as_: Some(vec!["foo".to_string(), "bar".to_string()]),
-        offset: None,
-        extra: Default::default(),
-    };
+#[cfg(test)]
+mod test_stack_with_group_sort {
+    use vegafusion_core::spec::transform::stack::StackOffsetSpec;
+    use super::*;
 
-    let transform_specs = vec![TransformSpec::Stack(stack_spec)];
+    #[rstest(
+        offset,
+        case(None),
+        case(Some(StackOffsetSpec::Zero))
+    )]
+    fn test(offset: Option<StackOffsetSpec>) {
+        let dataset = vega_json_dataset("penguins");
 
-    let comp_config = Default::default();
-    let eq_config = TablesEqualConfig {
-        row_order: true,
-        ..Default::default()
-    };
+        let stack_spec = StackTransformSpec {
+            field: Field::String("Body Mass (g)".to_string()),
+            groupby: Some(vec![Field::String("Species".to_string())]),
+            sort: Some(CompareSpec {
+                field: StringOrStringList::StringList(vec![
+                    "Sex".to_string(),
+                    "Flipper Length (mm)".to_string(),
+                ]),
+                order: Some(SortOrderOrList::SortOrderList(vec![
+                    SortOrderSpec::Ascending,
+                    SortOrderSpec::Descending,
+                ])),
+            }),
+            as_: Some(vec!["foo".to_string(), "bar".to_string()]),
+            offset,
+            extra: Default::default(),
+        };
 
-    check_transform_evaluation(
-        &dataset,
-        transform_specs.as_slice(),
-        &comp_config,
-        &eq_config,
-    );
+        let transform_specs = vec![TransformSpec::Stack(stack_spec)];
+
+        let comp_config = Default::default();
+        let eq_config = TablesEqualConfig {
+            row_order: true,
+            ..Default::default()
+        };
+
+        check_transform_evaluation(
+            &dataset,
+            transform_specs.as_slice(),
+            &comp_config,
+            &eq_config,
+        );
+    }
+
+    #[test]
+    fn test_marker() {} // Help IDE detect test module
 }
 
-#[test]
-fn test_stack_with_group_sort_negative() {
-    let dataset = vega_json_dataset("penguins");
 
-    let formula_spec = FormulaTransformSpec {
-        expr: "(datum['Body Mass (g)'] || 0) - 4000".to_string(),
-        as_: "Body Mass (g)".to_string(),
-        extra: Default::default(),
-    };
+#[cfg(test)]
+mod test_stack_with_group_sort_negative {
+    use vegafusion_core::spec::transform::stack::StackOffsetSpec;
+    use super::*;
 
-    let stack_spec = StackTransformSpec {
-        field: Field::String("Body Mass (g)".to_string()),
-        groupby: Some(vec![Field::String("Species".to_string())]),
-        sort: Some(CompareSpec {
-            field: StringOrStringList::StringList(vec![
-                "Sex".to_string(),
-                "Flipper Length (mm)".to_string(),
-            ]),
-            order: Some(SortOrderOrList::SortOrderList(vec![
-                SortOrderSpec::Ascending,
-                SortOrderSpec::Descending,
-            ])),
-        }),
-        as_: Some(vec!["foo".to_string(), "bar".to_string()]),
-        offset: None,
-        extra: Default::default(),
-    };
+    #[rstest(
+        offset,
+        case(None),
+        case(Some(StackOffsetSpec::Zero))
+    )]
+    fn test(offset: Option<StackOffsetSpec>) {
+        let dataset = vega_json_dataset("penguins");
 
-    let transform_specs = vec![
-        TransformSpec::Formula(formula_spec),
-        TransformSpec::Stack(stack_spec),
-    ];
+        let formula_spec = FormulaTransformSpec {
+            expr: "(datum['Body Mass (g)'] || 0) - 4000".to_string(),
+            as_: "Body Mass (g)".to_string(),
+            extra: Default::default(),
+        };
 
-    let comp_config = Default::default();
-    let eq_config = TablesEqualConfig {
-        row_order: true,
-        ..Default::default()
-    };
+        let stack_spec = StackTransformSpec {
+            field: Field::String("Body Mass (g)".to_string()),
+            groupby: Some(vec![Field::String("Species".to_string())]),
+            sort: Some(CompareSpec {
+                field: StringOrStringList::StringList(vec![
+                    "Sex".to_string(),
+                    "Flipper Length (mm)".to_string(),
+                ]),
+                order: Some(SortOrderOrList::SortOrderList(vec![
+                    SortOrderSpec::Ascending,
+                    SortOrderSpec::Descending,
+                ])),
+            }),
+            as_: Some(vec!["foo".to_string(), "bar".to_string()]),
+            offset,
+            extra: Default::default(),
+        };
 
-    check_transform_evaluation(
-        &dataset,
-        transform_specs.as_slice(),
-        &comp_config,
-        &eq_config,
-    );
+        let transform_specs = vec![
+            TransformSpec::Formula(formula_spec),
+            TransformSpec::Stack(stack_spec),
+        ];
+
+        let comp_config = Default::default();
+        let eq_config = TablesEqualConfig {
+            row_order: true,
+            ..Default::default()
+        };
+
+        check_transform_evaluation(
+            &dataset,
+            transform_specs.as_slice(),
+            &comp_config,
+            &eq_config,
+        );
+    }
+
+    #[test]
+    fn test_marker() {} // Help IDE detect test module
 }
