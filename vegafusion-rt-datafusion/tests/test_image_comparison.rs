1b55d5a632df5641f763294e176c9c2ca8372800
Jon Mease <jon.mease.dev@gmail.com>
2022-08-04 16:08:52 -0400

    Add failing test for bug 153

--- vegafusion-rt-datafusion/tests/test_image_comparison.rs
+++ vegafusion-rt-datafusion/tests/test_image_comparison.rs
@@ -118,7 +118,8 @@ mod test_custom_specs {
         case("custom/escaped_column_name1", 0.001),
         case("custom/layered_movies", 0.001),
         case("custom/shipping_mixed_scales", 0.001),
-        case("custom/datum_color", 0.001)
+        case("custom/datum_color", 0.001),
+        case("custom/bug_153", 0.001),
     )]
     fn test_image_comparison(spec_name: &str, tolerance: f64) {
         println!("spec_name: {}", spec_name);
