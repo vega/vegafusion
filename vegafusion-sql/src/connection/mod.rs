0654d59a92ee43d1e08d6daca00e2cb72cfa91a9
Jon Mease <jon.mease.dev@gmail.com>
2022-08-05 19:08:32 -0400

    refinements

--- vegafusion-sql/src/connection/mod.rs
+++ vegafusion-sql/src/connection/mod.rs
@@ -1,10 +1,13 @@
 pub mod sqlite;
 
 use std::collections::HashMap;
+use std::sync::Arc;
 use vegafusion_core::arrow::datatypes::Schema;
 use vegafusion_core::data::table::VegaFusionTable;
 use vegafusion_core::error::Result;
 use async_trait::async_trait;
+use datafusion::datasource::empty::EmptyTable;
+use datafusion::prelude::SessionContext;
 use sqlgen::ast::Query;
 
 
@@ -17,4 +20,13 @@ pub trait SqlDatabaseConnection: Send + Sync {
     ) -> Result<VegaFusionTable>;
 
     fn tables(&self) -> Result<HashMap<String, Schema>>;
+
+    fn session_context(&self) -> Result<SessionContext> {
+        let ctx = SessionContext::new();
+        for (table_name, schema) in self.tables()? {
+            let table = EmptyTable::new(Arc::new(schema));
+            ctx.register_table(table_name.as_str(), Arc::new(table));
+        }
+        Ok(ctx)
+    }
 }
--- vegafusion-sql/src/dataframe/mod.rs
+++ vegafusion-sql/src/dataframe/mod.rs
@@ -1,11 +1,13 @@
 use std::sync::Arc;
-use vegafusion_core::arrow::datatypes::SchemaRef;
+use datafusion::prelude::SessionContext;
+use vegafusion_core::arrow::datatypes::{Schema, SchemaRef};
 use vegafusion_core::data::table::VegaFusionTable;
-use vegafusion_core::error::{Result, ResultWithContext};
-use sqlgen::ast::{OrderByExpr, Query, TableAlias};
+use vegafusion_core::error::{Result, ResultWithContext, VegaFusionError};
+use sqlgen::ast::{OrderByExpr, Query, SetExpr, TableAlias, TableFactor, TableWithJoins};
 use sqlgen::ast::Expr;
 use sqlgen::ast::Ident;
 use sqlgen::ast::{Cte, With};
+use sqlgen::dialect::DialectDisplay;
 use sqlgen::parser::Parser;
 use crate::connection::SqlDatabaseConnection;
 
@@ -15,7 +17,8 @@ pub struct SqlDataFrame {
     prefix: String,
     schema: SchemaRef,
     ctes: Vec<Query>,
-    conn: Arc<dyn SqlDatabaseConnection>
+    conn: Arc<dyn SqlDatabaseConnection>,
+    session_context: Arc<SessionContext>
 }
 
 impl SqlDataFrame {
@@ -33,41 +36,42 @@ impl SqlDataFrame {
             prefix: "t_".to_string(),
             ctes: vec![query],
             schema: Arc::new(schema.clone()),
+            session_context: Arc::new(conn.session_context()?),
             conn,
         })
     }
 
-    pub fn _next_cte_name(&self) -> String {
-        format!("{}{}", self.prefix, self.ctes.len())
+    pub fn as_query(&self) -> Query {
+        query_chain_to_cte(self.ctes.as_slice(), &self.prefix)
     }
 
-    pub fn query(&self) -> Query {
-        let cte_tables: Vec<_> = self.ctes[0..self.ctes.len()-1].iter().enumerate().map(|(i, query)| {
-            let cte_name = format!("{}{}", self.prefix, i+1);
-            Cte {
-                alias: TableAlias { name: Ident { value: cte_name, quote_style: None }, columns: vec![] },
-                query: query.clone(),
-                from: None
-            }
-        }).collect();
+    fn chain_query_str(&self, query: &str) -> Result<Arc<Self>> {
+        let query_ast = Parser::parse_sql_query(query).map_err(|err| {
+            VegaFusionError::internal(err.to_string())
+        })?;
+        self.chain_query(query_ast)
+    }
 
-        let mut final_query = self.ctes[self.ctes.len() - 1].clone();
-        final_query.with = if cte_tables.is_empty() {
-            None
-        } else {
-            Some(With {
-                recursive: false,
-                cte_tables
-            })
-        };
+    fn chain_query(&self, query: Query) -> Result<Arc<Self>> {
+        let mut new_ctes = self.ctes.clone();
+        new_ctes.push(query);
 
-        final_query
+        let combined_query = query_chain_to_cte(new_ctes.as_slice(), &self.prefix);
+        let query_str = combined_query.sql(&Default::default()).expect("Failed to create query");
+        let logical_plan = self.session_context.create_logical_plan(&query_str)?;
+        let new_schema: Schema = logical_plan.schema().as_ref().into();
+
+        Ok(Arc::new(SqlDataFrame {
+            prefix: self.prefix.clone(),
+            schema: Arc::new(new_schema),
+            ctes: new_ctes,
+            conn: self.conn.clone(),
+            session_context: self.session_context.clone(),
+        }))
     }
 
     fn sort(&self, columns: &[&str]) -> Result<Arc<Self>> {
-        let new_schema = self.schema.clone();
-        let name = self._next_cte_name();
-        let mut query = Parser::parse_sql_query(&format!("select * from {}", name)).unwrap();
+        let mut query = Parser::parse_sql_query("select * from parent").unwrap();
 
         let order_by_expr: Vec<_> = columns.iter().map(|col| {
             OrderByExpr {
@@ -78,19 +82,75 @@ impl SqlDataFrame {
         }).collect();
 
         query.order_by = order_by_expr;
-        let mut new_ctes = self.ctes.clone();
-        new_ctes.push(query);
-
-        Ok(Arc::new(SqlDataFrame {
-            prefix: self.prefix.clone(),
-            schema: new_schema,
-            ctes: new_ctes,
-            conn: self.conn.clone()
-        }))
+        self.chain_query(query)
     }
 
     async fn collect(&self) -> Result<VegaFusionTable> {
-        self.conn.fetch_query(&self.query(), &self.schema).await
+        self.conn.fetch_query(&self.as_query(), &self.schema).await
+    }
+}
+
+fn query_chain_to_cte(queries: &[Query], prefix: &str) -> Query {
+    // Rename the "parent" table in each query to refer to prior CTE stage
+    let mut queries = Vec::from(queries);
+    queries.iter_mut().enumerate().for_each(|(i, query)| {
+        let parent_cte_name = format!("{}{}", prefix, i);
+        rename_table_in_query(query, "parent", &parent_cte_name);
+    });
+
+    // Build vector of CTE AST nodes for all but the last query
+    let cte_tables: Vec<_> = queries.iter().enumerate().map(|(i, query)| {
+        let this_cte_name = format!("{}{}", prefix, i+1);
+        Cte {
+            alias: TableAlias { name: Ident { value: this_cte_name, quote_style: None }, columns: vec![] },
+            query: query.clone(),
+            from: None
+        }
+    }).collect();
+
+    // The final query becomes the top-level query with CTEs attached to it
+    let mut final_query = queries[queries.len() - 1].clone();
+    final_query.with = if cte_tables.is_empty() {
+        None
+    } else {
+        Some(With {
+            recursive: false,
+            cte_tables
+        })
+    };
+
+    final_query
+}
+
+fn rename_table_in_query(query: &mut Query, old_name: &str, new_name: &str) {
+    rename_tables_in_set_expr(&mut query.body, old_name, new_name)
+
+}
+
+fn rename_tables_in_set_expr(set_expr: &mut SetExpr, old_name: &str, new_name: &str) {
+    match set_expr {
+        SetExpr::Select(select) => {
+            let from: &mut Vec<TableWithJoins> = &mut select.from;
+            for twj in from.iter_mut() {
+                rename_tables_in_table_factor(&mut twj.relation, old_name, new_name);
+                for join in twj.joins.iter_mut() {
+                    rename_tables_in_table_factor(&mut join.relation, old_name, new_name);
+                }
+            }
+        }
+        SetExpr::SetOperation { left, right, .. } => {
+            rename_tables_in_set_expr(left, old_name, new_name);
+            rename_tables_in_set_expr(right, old_name, new_name);
+        }
+        _ => {}
+    }
+}
+
+fn rename_tables_in_table_factor(table_factor: &mut TableFactor, old_name: &str, new_name: &str) {
+    if let TableFactor::Table { name, .. } = table_factor {
+        if name.0.len() == 1 && &name.0[0].value == old_name {
+            name.0[0].value = new_name.to_string();
+        }
     }
 }
 
@@ -122,12 +182,14 @@ mod test {
         );
 
         let df = SqlDataFrame::try_new(Arc::new(conn), "stock").unwrap();
+        println!("{:#?}", df.schema);
 
-        let df = df.sort(vec!["index"].as_slice()).unwrap();
+        // Transform query
+        let df = df.chain_query_str("select date, symbol, price, price * 2 as dbl_price from parent order by date").unwrap();
+        println!("{:#?}", df.schema);
 
-        let df = df.sort(vec!["symbol", "price"].as_slice()).unwrap();
-
-        let s = df.query().sql(&Default::default()).unwrap();
+        // Extract SQL in
+        let s = df.as_query().sql(&Default::default()).unwrap();
         println!("{}", s);
 
         let table = df.collect().await.unwrap();
