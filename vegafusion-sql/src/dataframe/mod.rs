6bb0920f6171478966a5ba087123d7fcb5e3f65f
Jon Mease <jon.mease.dev@gmail.com>
2022-08-11 12:14:36 -0400

    Remove parent table magic Have SqlDataFrame provide the name of the parent table to use when buiding queries

--- vegafusion-sql/src/dataframe/mod.rs
+++ vegafusion-sql/src/dataframe/mod.rs
@@ -1,16 +1,15 @@
-use std::sync::Arc;
+use crate::connection::SqlDatabaseConnection;
 use datafusion::prelude::SessionContext;
-use vegafusion_core::arrow::datatypes::{Schema, SchemaRef};
-use vegafusion_core::data::table::VegaFusionTable;
-use vegafusion_core::error::{Result, ResultWithContext, VegaFusionError};
-use sqlgen::ast::{OrderByExpr, Query, SetExpr, TableAlias, TableFactor, TableWithJoins};
 use sqlgen::ast::Expr;
 use sqlgen::ast::Ident;
 use sqlgen::ast::{Cte, With};
+use sqlgen::ast::{OrderByExpr, Query, SetExpr, TableAlias, TableFactor, TableWithJoins};
 use sqlgen::dialect::{Dialect, DialectDisplay};
 use sqlgen::parser::Parser;
-use crate::connection::SqlDatabaseConnection;
-
+use std::sync::Arc;
+use vegafusion_core::arrow::datatypes::{Schema, SchemaRef};
+use vegafusion_core::data::table::VegaFusionTable;
+use vegafusion_core::error::{Result, ResultWithContext, VegaFusionError};
 
 #[derive(Clone)]
 pub struct SqlDataFrame {
@@ -18,19 +17,25 @@ pub struct SqlDataFrame {
     schema: SchemaRef,
     ctes: Vec<Query>,
     conn: Arc<dyn SqlDatabaseConnection>,
-    session_context: Arc<SessionContext>
+    session_context: Arc<SessionContext>,
 }
 
 impl SqlDataFrame {
     pub fn try_new(conn: Arc<dyn SqlDatabaseConnection>, table: &str) -> Result<Self> {
         let tables = conn.tables()?;
-        let schema = tables.get(table).cloned().with_context(
-            || format!("Connection has no table named {}", table)
-        )?;
+        let schema = tables
+            .get(table)
+            .cloned()
+            .with_context(|| format!("Connection has no table named {}", table))?;
         // Should quote column names
-        let columns: Vec<_> = schema.fields().iter().map(|f| format!("\"{}\"", f.name())).collect();
+        let columns: Vec<_> = schema
+            .fields()
+            .iter()
+            .map(|f| format!("\"{}\"", f.name()))
+            .collect();
         let select_items = columns.join(", ");
-        let query = Parser::parse_sql_query(&format!("select {} from {}", select_items, table)).unwrap();
+        let query =
+            Parser::parse_sql_query(&format!("select {} from {}", select_items, table)).unwrap();
 
         Ok(Self {
             prefix: "t_".to_string(),
@@ -41,14 +46,17 @@ impl SqlDataFrame {
         })
     }
 
+    pub fn parent_name(&self) -> String {
+        parent_cte_name_for_index(&self.prefix, self.ctes.len())
+    }
+
     pub fn as_query(&self) -> Query {
         query_chain_to_cte(self.ctes.as_slice(), &self.prefix)
     }
 
     fn chain_query_str(&self, query: &str) -> Result<Arc<Self>> {
-        let query_ast = Parser::parse_sql_query(query).map_err(|err| {
-            VegaFusionError::internal(err.to_string())
-        })?;
+        let query_ast = Parser::parse_sql_query(query)
+            .map_err(|err| VegaFusionError::internal(err.to_string()))?;
         self.chain_query(query_ast)
     }
 
@@ -58,8 +66,8 @@ impl SqlDataFrame {
 
         let combined_query = query_chain_to_cte(new_ctes.as_slice(), &self.prefix);
 
-        // First, the combined query to a string using the connection's dialect to make sure
-        // that it is supported by the connection
+        // First, convert the combined query to a string using the connection's dialect to make
+        // sure that it is supported by the connection
         combined_query.sql(self.conn.dialect())?;
 
         // Now convert to string in the DataFusion dialect for schema inference
@@ -76,44 +84,40 @@ impl SqlDataFrame {
         }))
     }
 
-    fn sort(&self, columns: &[&str]) -> Result<Arc<Self>> {
-        let mut query = Parser::parse_sql_query("select * from parent").unwrap();
-
-        let order_by_expr: Vec<_> = columns.iter().map(|col| {
-            OrderByExpr {
-                expr: Expr::Identifier(Ident { value: col.to_string(), quote_style: None }),
-                asc: None,
-                nulls_first: None
-            }
-        }).collect();
-
-        query.order_by = order_by_expr;
-        self.chain_query(query)
-    }
-
     async fn collect(&self) -> Result<VegaFusionTable> {
         let query_string = self.as_query().sql(self.conn.dialect())?;
         self.conn.fetch_query(&query_string, &self.schema).await
     }
 }
 
-fn query_chain_to_cte(queries: &[Query], prefix: &str) -> Query {
-    // Rename the "parent" table in each query to refer to prior CTE stage
-    let mut queries = Vec::from(queries);
-    queries.iter_mut().enumerate().for_each(|(i, query)| {
-        let parent_cte_name = format!("{}{}", prefix, i);
-        rename_table_in_query(query, "parent", &parent_cte_name);
-    });
+fn cte_name_for_index(prefix: &str, index: usize) -> String {
+    format!("{}{}", prefix, index)
+}
 
+fn parent_cte_name_for_index(prefix: &str, index: usize) -> String {
+    cte_name_for_index(prefix, index - 1)
+}
+
+fn query_chain_to_cte(queries: &[Query], prefix: &str) -> Query {
     // Build vector of CTE AST nodes for all but the last query
-    let cte_tables: Vec<_> = queries.iter().enumerate().map(|(i, query)| {
-        let this_cte_name = format!("{}{}", prefix, i+1);
-        Cte {
-            alias: TableAlias { name: Ident { value: this_cte_name, quote_style: None }, columns: vec![] },
-            query: query.clone(),
-            from: None
-        }
-    }).collect();
+    let cte_tables: Vec<_> = queries[..queries.len() - 1]
+        .iter()
+        .enumerate()
+        .map(|(i, query)| {
+            let this_cte_name = cte_name_for_index(prefix, i);
+            Cte {
+                alias: TableAlias {
+                    name: Ident {
+                        value: this_cte_name,
+                        quote_style: None,
+                    },
+                    columns: vec![],
+                },
+                query: query.clone(),
+                from: None,
+            }
+        })
+        .collect();
 
     // The final query becomes the top-level query with CTEs attached to it
     let mut final_query = queries[queries.len() - 1].clone();
@@ -122,54 +126,22 @@ fn query_chain_to_cte(queries: &[Query], prefix: &str) -> Query {
     } else {
         Some(With {
             recursive: false,
-            cte_tables
+            cte_tables,
         })
     };
 
     final_query
 }
 
-fn rename_table_in_query(query: &mut Query, old_name: &str, new_name: &str) {
-    rename_tables_in_set_expr(&mut query.body, old_name, new_name)
-
-}
-
-fn rename_tables_in_set_expr(set_expr: &mut SetExpr, old_name: &str, new_name: &str) {
-    match set_expr {
-        SetExpr::Select(select) => {
-            let from: &mut Vec<TableWithJoins> = &mut select.from;
-            for twj in from.iter_mut() {
-                rename_tables_in_table_factor(&mut twj.relation, old_name, new_name);
-                for join in twj.joins.iter_mut() {
-                    rename_tables_in_table_factor(&mut join.relation, old_name, new_name);
-                }
-            }
-        }
-        SetExpr::SetOperation { left, right, .. } => {
-            rename_tables_in_set_expr(left, old_name, new_name);
-            rename_tables_in_set_expr(right, old_name, new_name);
-        }
-        _ => {}
-    }
-}
-
-fn rename_tables_in_table_factor(table_factor: &mut TableFactor, old_name: &str, new_name: &str) {
-    if let TableFactor::Table { name, .. } = table_factor {
-        if name.0.len() == 1 && &name.0[0].value == old_name {
-            name.0[0].value = new_name.to_string();
-        }
-    }
-}
-
 #[cfg(test)]
 mod test {
-    use std::sync::Arc;
-    use sqlgen::dialect::{Dialect, DialectDisplay};
-    use sqlx::SqlitePool;
-    use vegafusion_core::arrow::datatypes::{DataType, Field, Schema};
-    use vegafusion_rt_datafusion::data::table::VegaFusionTableUtils;
     use crate::connection::sqlite::SqLiteConnection;
     use crate::dataframe::SqlDataFrame;
+    use sqlgen::dialect::{Dialect, DialectDisplay};
+    use sqlx::SqlitePool;
+    use std::sync::Arc;
+    use vegafusion_core::arrow::datatypes::{DataType, Field, Schema};
+    use vegafusion_rt_datafusion::data::table::VegaFusionTableUtils;
 
     #[tokio::test]
     async fn try_it() {
@@ -184,16 +156,27 @@ mod test {
             .await
             .unwrap();
 
-        let conn = SqLiteConnection::new(
-            Arc::new(pool), "stock", &schema
-        );
+        let conn = SqLiteConnection::new(Arc::new(pool), "stock", &schema);
 
         let df = SqlDataFrame::try_new(Arc::new(conn), "stock").unwrap();
         println!("{:#?}", df.schema);
 
         // Transform query
-        let df = df.chain_query_str("select date, symbol, price, price * 2 as dbl_price from parent order by date").unwrap();
+        let parent = df.parent_name();
+        let df = df
+            .chain_query_str(&format!(
+                "select date, symbol, price, price * 2 as dbl_price from {parent}",
+                parent = df.parent_name()
+            ))
+            .unwrap();
         println!("{:#?}", df.schema);
+
+        let df = df
+            .chain_query_str(&format!(
+                "select * from {parent} order by date",
+                parent = df.parent_name()
+            ))
+            .unwrap();
 
         // Extract SQL in the sqlite dialect
         let s = df.as_query().sql(&Dialect::sqlite()).unwrap();
--- vegafusion-sql/tests/test_query_sqlite.rs
+++ vegafusion-sql/tests/test_query_sqlite.rs
@@ -4,6 +4,7 @@ use sqlx::SqlitePool;
 use vegafusion_core::arrow::datatypes::DataType;
 use vegafusion_rt_datafusion::data::table::VegaFusionTableUtils;
 use sqlgen::ast::{Query, SetExpr, TableFactor, TableWithJoins, Ident, ObjectName, Select, SelectItem};
+use sqlgen::dialect::DialectDisplay;
 use sqlgen::parser::Parser;
 use vegafusion_sql::connection::SqlDatabaseConnection;
 use vegafusion_sql::connection::sqlite::SqLiteConnection;
@@ -27,8 +28,9 @@ async fn try_it() {
 
     // let query = Query::select_star_from("stock");
     let query = Parser::parse_sql_query("SELECT symbol, price from stock").unwrap();
+    let query_str = query.sql(&conn.dialect()).unwrap();
 
-    let result = conn.fetch_query(&query, &schema).await.unwrap();
+    let result = conn.fetch_query(&query_str, &schema).await.unwrap();
 
     println!("{}", result.pretty_format(Some(10)).unwrap());
 }
