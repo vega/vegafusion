5cc82a48be469fe892eadf362238180626dd87dc
Jon Mease <jon.mease.dev@gmail.com>
2022-08-18 08:48:26 -0400

    Add sql aggregate transform support

--- vegafusion-rt-datafusion/src/transform/aggregate.rs
+++ vegafusion-rt-datafusion/src/transform/aggregate.rs
@@ -17,6 +17,7 @@ use crate::sql::dataframe::SqlDataFrame;
 use async_trait::async_trait;
 use datafusion_expr::{aggregate_function, BuiltInWindowFunction, WindowFunction};
 use std::sync::Arc;
+use datafusion::common::DFSchema;
 use vegafusion_core::arrow::datatypes::DataType;
 use vegafusion_core::error::{Result, ResultWithContext, VegaFusionError};
 use vegafusion_core::proto::gen::transforms::{Aggregate, AggregateOp};
@@ -30,122 +31,8 @@ impl TransformTrait for Aggregate {
         dataframe: Arc<DataFrame>,
         _config: &CompilationConfig,
     ) -> Result<(Arc<DataFrame>, Vec<TaskValue>)> {
-        // DataFusion does not allow repeated (field, op) combinations in an aggregate expression,
-        // so if there are duplicates we need to use a projection after the aggregation to alias
-        // the desired column
-        let mut agg_aliases: HashMap<(Option<String>, i32), String> = HashMap::new();
-
-        // Initialize vec of final projections with the grouping fields
-        let mut projections: Vec<_> = self.groupby.iter().map(|f| col(f)).collect();
-
-        for (i, (field, op_code)) in self.fields.iter().zip(self.ops.iter()).enumerate() {
-            let op = AggregateOp::from_i32(*op_code).unwrap();
-
-            let column = if *op_code == AggregateOp::Count as i32 {
-                // In Vega, the provided column is always ignored if op is 'count'.
-                None
-            } else {
-                match field.as_str() {
-                    "" => {
-                        return Err(VegaFusionError::specification(&format!(
-                            "Null field is not allowed for {:?} op",
-                            op
-                        )))
-                    }
-                    column => Some(column.to_string()),
-                }
-            };
-
-            // Apply alias
-            let alias = if let Some(alias) = self.aliases.get(i).filter(|a| !a.is_empty()) {
-                // Alias is a non-empty string
-                alias.clone()
-            } else if field.is_empty() {
-                op_name(op).to_string()
-            } else {
-                format!("{}_{}", op_name(op), field,)
-            };
-
-            let key = (column, *op_code);
-            if let Some(agg_alias) = agg_aliases.get(&key) {
-                // We're already going to preform the aggregation, so alias result
-                projections.push(col(agg_alias).alias(&alias));
-            } else {
-                projections.push(col(&alias));
-                agg_aliases.insert(key, alias);
-            }
-        }
-
-        let mut agg_exprs = Vec::new();
-
-        for ((col_name, op_code), alias) in agg_aliases {
-            let op = AggregateOp::from_i32(op_code).unwrap();
-            let column = if let Some(col_name) = col_name {
-                col(&col_name)
-            } else {
-                lit(0i32)
-            };
-
-            let numeric_column = || {
-                to_numeric(column.clone(), dataframe.schema()).unwrap_or_else(|_| {
-                    panic!("Failed to convert column {:?} to numeric data type", column)
-                })
-            };
-
-            let agg_expr = match op {
-                AggregateOp::Count => count(column),
-                AggregateOp::Mean | AggregateOp::Average => avg(numeric_column()),
-                AggregateOp::Min => min(numeric_column()),
-                AggregateOp::Max => max(numeric_column()),
-                AggregateOp::Sum => sum(numeric_column()),
-                AggregateOp::Variance => Expr::AggregateFunction {
-                    fun: aggregate_function::AggregateFunction::Variance,
-                    distinct: false,
-                    args: vec![numeric_column()],
-                },
-                AggregateOp::Variancep => Expr::AggregateFunction {
-                    fun: aggregate_function::AggregateFunction::VariancePop,
-                    distinct: false,
-                    args: vec![numeric_column()],
-                },
-                AggregateOp::Stdev => Expr::AggregateFunction {
-                    fun: aggregate_function::AggregateFunction::Stddev,
-                    distinct: false,
-                    args: vec![numeric_column()],
-                },
-                AggregateOp::Stdevp => Expr::AggregateFunction {
-                    fun: aggregate_function::AggregateFunction::StddevPop,
-                    distinct: false,
-                    args: vec![numeric_column()],
-                },
-                AggregateOp::Valid => {
-                    let valid = Expr::Cast {
-                        expr: Box::new(Expr::IsNotNull(Box::new(column))),
-                        data_type: DataType::UInt64,
-                    };
-                    sum(valid)
-                }
-                AggregateOp::Missing => {
-                    let missing = Expr::Cast {
-                        expr: Box::new(Expr::IsNull(Box::new(column))),
-                        data_type: DataType::UInt64,
-                    };
-                    sum(missing)
-                }
-                AggregateOp::Distinct => count_distinct(column),
-                _ => {
-                    return Err(VegaFusionError::specification(&format!(
-                        "Unsupported aggregation op: {:?}",
-                        op
-                    )))
-                }
-            };
-
-            // Apply alias
-            let agg_expr = agg_expr.alias(&alias);
-
-            agg_exprs.push(agg_expr)
-        }
+        let group_exprs: Vec<_> = self.groupby.iter().map(|c| col(c)).collect();
+        let (mut agg_exprs, projections) = get_agg_and_proj_exprs(self, dataframe.schema())?;
 
         let dataframe = if !self.groupby.is_empty() {
             //  Add row_number column that we can sort by
@@ -166,7 +53,6 @@ impl TransformTrait for Aggregate {
             dataframe
         };
 
-        let group_exprs: Vec<_> = self.groupby.iter().map(|c| col(c)).collect();
         let mut grouped_dataframe = dataframe
             .aggregate(group_exprs, agg_exprs)
             .with_context(|| "Failed to perform aggregate transform".to_string())?;
@@ -187,11 +73,146 @@ impl TransformTrait for Aggregate {
 
     async fn eval_sql(
         &self,
-        _dataframe: Arc<SqlDataFrame>,
+        dataframe: Arc<SqlDataFrame>,
         _config: &CompilationConfig,
     ) -> Result<(Arc<SqlDataFrame>, Vec<TaskValue>)> {
-        Err(VegaFusionError::sql_not_supported(format!(
-            "Aggregate transform does not support SQL evaluation"
-        )))
+
+        let group_exprs: Vec<_> = self.groupby.iter().map(|c| col(c)).collect();
+        let (mut agg_exprs, projections) = get_agg_and_proj_exprs(self, &dataframe.schema_df())?;
+
+        let grouped_dataframe = dataframe.aggregate(group_exprs, agg_exprs)?;
+        let grouped_dataframe = grouped_dataframe.select(projections)?;
+
+        Ok((grouped_dataframe, Vec::new()))
     }
 }
+
+
+fn get_agg_and_proj_exprs(tx: &Aggregate, schema: &DFSchema) -> Result<(Vec<Expr>, Vec<Expr>)> {
+    // DataFusion does not allow repeated (field, op) combinations in an aggregate expression,
+    // so if there are duplicates we need to use a projection after the aggregation to alias
+    // the desired column
+    let mut agg_aliases: HashMap<(Option<String>, i32), String> = HashMap::new();
+
+    // Initialize vec of final projections with the grouping fields
+    let mut projections: Vec<_> = tx.groupby.iter().map(|f| col(f)).collect();
+
+    for (i, (field, op_code)) in tx.fields.iter().zip(tx.ops.iter()).enumerate() {
+        let op = AggregateOp::from_i32(*op_code).unwrap();
+
+        let column = if *op_code == AggregateOp::Count as i32 {
+            // In Vega, the provided column is always ignored if op is 'count'.
+            None
+        } else {
+            match field.as_str() {
+                "" => {
+                    return Err(VegaFusionError::specification(&format!(
+                        "Null field is not allowed for {:?} op",
+                        op
+                    )))
+                }
+                column => Some(column.to_string()),
+            }
+        };
+
+        // Apply alias
+        let alias = if let Some(alias) = tx.aliases.get(i).filter(|a| !a.is_empty()) {
+            // Alias is a non-empty string
+            alias.clone()
+        } else if field.is_empty() {
+            op_name(op).to_string()
+        } else {
+            format!("{}_{}", op_name(op), field, )
+        };
+
+        let key = (column, *op_code);
+        if let Some(agg_alias) = agg_aliases.get(&key) {
+            // We're already going to preform the aggregation, so alias result
+            projections.push(col(agg_alias).alias(&alias));
+        } else {
+            projections.push(col(&alias));
+            agg_aliases.insert(key, alias);
+        }
+    }
+
+    let mut agg_exprs = Vec::new();
+
+    for ((col_name, op_code), alias) in agg_aliases {
+        let op = AggregateOp::from_i32(op_code).unwrap();
+
+        let agg_expr = make_aggr_expr(col_name, &op, schema)?;
+
+        // Apply alias
+        let agg_expr = agg_expr.alias(&alias);
+
+        agg_exprs.push(agg_expr)
+    }
+    Ok((agg_exprs, projections))
+}
+
+
+
+fn make_aggr_expr(col_name: Option<String>, op: &AggregateOp, schema: &DFSchema) -> Result<Expr> {
+    let column = if let Some(col_name) = col_name {
+        col(&col_name)
+    } else {
+        lit(0i32)
+    };
+
+    let numeric_column = || {
+        to_numeric(column.clone(), schema).unwrap_or_else(|_| {
+            panic!("Failed to convert column {:?} to numeric data type", column)
+        })
+    };
+
+    let agg_expr = match op {
+        AggregateOp::Count => count(column),
+        AggregateOp::Mean | AggregateOp::Average => avg(numeric_column()),
+        AggregateOp::Min => min(numeric_column()),
+        AggregateOp::Max => max(numeric_column()),
+        AggregateOp::Sum => sum(numeric_column()),
+        AggregateOp::Variance => Expr::AggregateFunction {
+            fun: aggregate_function::AggregateFunction::Variance,
+            distinct: false,
+            args: vec![numeric_column()],
+        },
+        AggregateOp::Variancep => Expr::AggregateFunction {
+            fun: aggregate_function::AggregateFunction::VariancePop,
+            distinct: false,
+            args: vec![numeric_column()],
+        },
+        AggregateOp::Stdev => Expr::AggregateFunction {
+            fun: aggregate_function::AggregateFunction::Stddev,
+            distinct: false,
+            args: vec![numeric_column()],
+        },
+        AggregateOp::Stdevp => Expr::AggregateFunction {
+            fun: aggregate_function::AggregateFunction::StddevPop,
+            distinct: false,
+            args: vec![numeric_column()],
+        },
+        AggregateOp::Valid => {
+            let valid = Expr::Cast {
+                expr: Box::new(Expr::IsNotNull(Box::new(column))),
+                data_type: DataType::UInt64,
+            };
+            sum(valid)
+        }
+        AggregateOp::Missing => {
+            let missing = Expr::Cast {
+                expr: Box::new(Expr::IsNull(Box::new(column))),
+                data_type: DataType::UInt64,
+            };
+            sum(missing)
+        }
+        AggregateOp::Distinct => count_distinct(column),
+        _ => {
+            return Err(VegaFusionError::specification(&format!(
+                "Unsupported aggregation op: {:?}",
+                op
+            )))
+        }
+    };
+    Ok(agg_expr)
+}
+
