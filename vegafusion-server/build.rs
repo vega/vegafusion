8dfd76310f574daf823138d1059f4806f3a3f245
Jon Mease <jonmmease@gmail.com>
2022-12-19 06:35:07 -0500

    Use protobuf_src in vegafusion-server

--- vegafusion-server/build.rs
+++ vegafusion-server/build.rs
@@ -1,4 +1,5 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    std::env::set_var("PROTOC", protobuf_src::protoc());
     tonic_build::compile_protos("proto/helloworld.proto")?;
     Ok(())
 }
