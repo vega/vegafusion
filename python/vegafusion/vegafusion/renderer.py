8cd7f5730fa53f043870ce9364f76c154e19f0f2
Jon Mease <jonmmease@gmail.com>
2022-12-26 09:48:09 -0500

    work around to avoid white background in dark theme

--- python/vegafusion/vegafusion/renderer.py
+++ python/vegafusion/vegafusion/renderer.py
@@ -7,6 +7,13 @@ def vegafusion_mime_renderer(spec, mimetype="html", embed_options=None):
     import vl_convert as vlc
     from . import altair_vl_version
     vega_spec = vlc.vegalite_to_vega(spec, vl_version=altair_vl_version())
+
+    # Remove background if non-default theme is active
+    # Not sure why this is needed, but otherwise dark theme will end up with a
+    # white background
+    if alt.themes.active != "default":
+        vega_spec.pop("background", None)
+
     inline_datasets = transformer.get_inline_datasets_for_spec(vega_spec)
     tx_vega_spec, warnings = runtime.pre_transform_spec(
         vega_spec,
