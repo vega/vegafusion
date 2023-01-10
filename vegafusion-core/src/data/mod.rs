8fd907b6f3f8f6972a287c074c96883dd64d4ed4
Jon Mease <jonmmease@gmail.com>
2023-01-10 08:30:40 -0500

    Add VegaFusionTable::with_ordering method

--- vegafusion-core/src/data/mod.rs
+++ vegafusion-core/src/data/mod.rs
@@ -1,4 +1,9 @@
+use arrow::datatypes::DataType;
+
 pub mod json_writer;
 pub mod scalar;
 pub mod table;
 pub mod tasks;
+
+const ORDER_COL: &str = "_vf_order";
+const ORDER_COL_DTYPE: DataType = DataType::UInt32;
