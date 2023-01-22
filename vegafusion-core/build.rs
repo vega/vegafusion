24c16f3d5742c2ec4972ead8d760bae2ab6814b6
Jon Mease <jonmmease@gmail.com>
2023-01-22 14:25:31 -0500

    1.0.1 Fixes (#233)
    * Use git dependency for DataFusion rather than patch. This way building from sdist will grab the right version
    
    * Make protobuf-src optional feature
    
    * Bump version to 1.0.1
    
    * Altair 5 compat by importing from altair package directly

--- vegafusion-core/build.rs
+++ vegafusion-core/build.rs
@@ -2,7 +2,7 @@ extern crate prost_build;
 
 fn main() {
     // Setup protoc environment variable
-    #[cfg(target_family = "unix")]
+    #[cfg(feature = "protobuf-src")]
     std::env::set_var("PROTOC", protobuf_src::protoc());
 
     let mut prost_config = prost_build::Config::new();
