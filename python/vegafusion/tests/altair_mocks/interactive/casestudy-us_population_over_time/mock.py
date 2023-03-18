3c600e1347c2cb29c994dfee7ec74c4a41c2d32f
Jon Mease <jonmmease@gmail.com>
2023-03-18 16:05:43 -0400

    Update Vega and Vega-Lite in VegaFusionWidget (#263)
    * Update Vega and Vega-Lite in VegaFusionWidget
    
    Use latest Altair in Python image tests
    
    * Update mock for Altair 5

--- python/vegafusion/tests/altair_mocks/interactive/casestudy-us_population_over_time/mock.py
+++ python/vegafusion/tests/altair_mocks/interactive/casestudy-us_population_over_time/mock.py
@@ -1,25 +1,28 @@
-# https://altair-viz.github.io/gallery/us_population_over_time.html
-
 import altair as alt
 from vega_datasets import data
 
 source = data.population.url
 
-pink_blue = alt.Scale(domain=('Male', 'Female'),
-                      range=["steelblue", "salmon"])
-
-slider = alt.binding_range(min=1900, max=2000, step=10)
-select_year = alt.selection_single(name="year", fields=['year'],
-                                   bind=slider, init={'year': 2000})
+select_year = alt.selection_point(
+    name="Year",
+    fields=["year"],
+    bind=alt.binding_range(min=1900, max=2000, step=10, name="Year"),
+    value={"year": 2000},
+)
 
 alt.Chart(source).mark_bar().encode(
-    x=alt.X('sex:N', title=None),
-    y=alt.Y('people:Q', scale=alt.Scale(domain=(0, 12000000))),
-    color=alt.Color('sex:N', scale=pink_blue),
-    column='age:O'
+    x=alt.X("sex:N", axis=alt.Axis(labels=False, title=None, ticks=False)),
+    y=alt.Y("people:Q", scale=alt.Scale(domain=(0, 12000000)), title="Population"),
+    color=alt.Color(
+        "sex:N",
+        scale=alt.Scale(domain=("Male", "Female"), range=["steelblue", "salmon"]),
+        title="Sex",
+    ),
+    column=alt.Column("age:O", title="Age"),
 ).properties(
-    width=18
-).add_selection(
+    width=20,
+    title="U.S. Population by Age and Sex"
+).add_params(
     select_year
 ).transform_calculate(
     "sex", alt.expr.if_(alt.datum.sex == 1, "Male", "Female")
