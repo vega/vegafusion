2e8e1bf1df6e757f70098b311ceacd410a1240e4
Jon Mease <jonmmease@gmail.com>
2023-04-12 15:49:21 -0400

    Remove unsupported wee_alloc dependency

--- vegafusion-wasm/src/lib.rs
+++ vegafusion-wasm/src/lib.rs
@@ -24,12 +24,6 @@ use vegafusion_core::task_graph::graph::ScopedVariable;
 use vegafusion_core::planning::plan::SpecPlan;
 use web_sys::Element;
 
-// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-// allocator.
-#[cfg(feature = "wee_alloc")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
 pub fn set_panic_hook() {
     // When the `console_error_panic_hook` feature is enabled, we can call the
     // `set_panic_hook` function at least once during initialization, and then
