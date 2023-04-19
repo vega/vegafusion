323165cba86da59a65f28ee036678db7910fbdd8
Jon Mease <jonmmease@gmail.com>
2023-04-19 08:51:30 -0400

    Handle empty strings in pivot by replacing with space

--- vegafusion-runtime/tests/test_transform_pivot.rs
+++ vegafusion-runtime/tests/test_transform_pivot.rs
@@ -24,6 +24,24 @@ fn medals() -> VegaFusionTable {
     .unwrap()
 }
 
+fn colors() -> VegaFusionTable {
+    VegaFusionTable::from_json(
+        &json!([
+            {"A": 1, "color": "red", "count": 1},
+            {"A": 1, "color": "green", "count": 2},
+            {"A": 1, "color": "", "count": 3},
+            {"A": 2, "color": "red", "count": 4},
+            {"A": 2, "color": "blue", "count": 5},
+            {"A": 3, "color": "green", "count": 6},
+            {"A": 3, "color": "red", "count": 7},
+            {"A": 3, "color": "", "count": 8},
+            {"A": 3, "color": "blue", "count": 9},
+        ]),
+        1024,
+    )
+    .unwrap()
+}
+
 #[cfg(test)]
 mod test_pivot_with_group {
     use crate::medals;
@@ -171,5 +189,54 @@ mod test_pivot_no_group_boolean {
             &comp_config,
             &eq_config,
         );
+    }
+}
+
+#[cfg(test)]
+mod test_pivot_with_empty_string {
+    use crate::colors;
+    use crate::util::check::eval_vegafusion_transforms;
+    use rstest::rstest;
+    use vegafusion_core::spec::transform::aggregate::AggregateOpSpec;
+    use vegafusion_core::spec::transform::pivot::PivotTransformSpec;
+    use vegafusion_core::spec::transform::TransformSpec;
+    use vegafusion_runtime::expression::compiler::config::CompilationConfig;
+
+    #[rstest(op, limit, case(Some(AggregateOpSpec::Sum), None))]
+    fn test(op: Option<AggregateOpSpec>, limit: Option<i32>) {
+        let dataset = colors();
+
+        let pivot_spec = PivotTransformSpec {
+            field: "color".to_string(),
+            value: "count".to_string(),
+            groupby: Some(vec!["A".to_string()]),
+            limit,
+            op,
+            extra: Default::default(),
+        };
+        let transform_specs = vec![TransformSpec::Pivot(pivot_spec)];
+
+        let compilation_config = CompilationConfig::default();
+
+        let (result_data, _result_signals) =
+            eval_vegafusion_transforms(&dataset, transform_specs.as_slice(), &compilation_config);
+
+        // Check representation of table
+        assert_eq!(
+            result_data.pretty_format(None).unwrap(),
+            "\
++-----+------+-------+-----+---+
+|     | blue | green | red | A |
++-----+------+-------+-----+---+
+| 3.0 | 0.0  | 2.0   | 1.0 | 1 |
+| 0.0 | 5.0  | 0.0   | 4.0 | 2 |
+| 8.0 | 9.0  | 6.0   | 7.0 | 3 |
++-----+------+-------+-----+---+"
+        );
+
+        // Check that first column is a single space (" "), rather than an empty string.
+        // Some SQL backends don't support empty string column names
+        let name0 = result_data.schema.fields[0].name();
+        assert_eq!(name0.as_str(), " ");
     }
 }
