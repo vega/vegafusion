9a9edec3c07e3a08d1f86bd7dec7f91dc3532f9a
Jon Mease <jonmmease@gmail.com>
2023-07-27 05:48:55 -0400

    Add two-column facet transform

--- vegafusion-runtime/src/transform/facet.rs
+++ vegafusion-runtime/src/transform/facet.rs
@@ -2,15 +2,15 @@ use crate::expression::compiler::config::CompilationConfig;
 use crate::transform::TransformTrait;
 use async_trait::async_trait;
 use datafusion_common::ScalarValue;
-use datafusion_expr::{ExprSchemable, lit, max};
+use datafusion_expr::lit;
 use std::sync::Arc;
 use vegafusion_common::arrow::record_batch::RecordBatch;
 use vegafusion_common::column::{flat_col, unescaped_col};
-use vegafusion_common::data::scalar::ScalarValueHelpers;
 use vegafusion_common::data::table::VegaFusionTable;
 use vegafusion_common::error::{Result, VegaFusionError};
 use vegafusion_common::escape::unescape_field;
 use vegafusion_core::arrow::datatypes::Schema;
+use vegafusion_core::proto::gen::transforms::Transform;
 use vegafusion_core::{proto::gen::transforms::Facet, task_graph::task_value::TaskValue};
 use vegafusion_dataframe::dataframe::DataFrame;
 
@@ -23,69 +23,23 @@ impl TransformTrait for Facet {
     ) -> Result<(Arc<dyn DataFrame>, Vec<TaskValue>)> {
         let (df, output_values) = match self.groupby.len() {
             1 => {
-                let group_field = unescape_field(&self.groupby[0]);
-                let group_col = unescaped_col(&self.groupby[0]);
-
-                let table = dataframe
-                    .aggregate(vec![group_col.clone()], vec![])
-                    .await?
-                    .collect()
-                    .await?;
-                let batch = table.to_record_batch()?;
-                let col_array = batch.column(0);
-                let col_values = (0..col_array.len())
-                    .map(|i| Ok(ScalarValue::try_from_array(col_array, i)?))
-                    .collect::<Result<Vec<_>>>()?;
-
-                let mut final_schema = Schema::empty();
-                let mut batches: Vec<RecordBatch> = Vec::new();
-                let mut output_values: Vec<TaskValue> = Vec::new();
-                for col_value in col_values {
-                    let filter = if col_value.is_null() {
-                        group_col.clone().is_null()
-                    } else {
-                        group_col.clone().eq(lit(col_value.clone()))
-                    };
-
-                    let mut filtered_df = dataframe.filter(filter).await?;
-
-                    // Apply transform pipeline
-                    for tx in &self.transform {
-                        let (tx_df, tx_out_vals) = tx.eval(filtered_df, config).await?;
-                        output_values.extend(tx_out_vals);
-                        filtered_df = tx_df;
-                    }
-
-                    // Add group value column back
-                    let mut selections = filtered_df
-                        .schema()
-                        .fields
-                        .iter()
-                        .filter_map(|f| {
-                            if f.name() == &group_field {
-                                None
-                            } else {
-                                Some(flat_col(f.name()))
-                            }
-                        })
-                        .collect::<Vec<_>>();
-                    selections.insert(0, lit(col_value).alias(&group_field));
-                    filtered_df = filtered_df.select(selections).await?;
-
-                    // Grab schema
-                    final_schema = filtered_df.schema();
-
-                    // evaluate to batches
-                    let final_table = filtered_df.collect().await?;
-                    batches.extend(final_table.batches);
-                }
-
-                let combined = VegaFusionTable::try_new(Arc::new(final_schema), batches)?;
-                let df = dataframe.connection().scan_arrow(combined).await?;
-                (df, output_values)
+                facet_one_column(
+                    dataframe,
+                    config,
+                    &self.groupby[0],
+                    self.transform.as_slice(),
+                )
+                .await?
             }
             2 => {
-                todo!("Facetting by two columns not yet implemented")
+                facet_two_columns(
+                    dataframe,
+                    config,
+                    &self.groupby[0],
+                    &self.groupby[1],
+                    self.transform.as_slice(),
+                )
+                .await?
             }
             i => {
                 return Err(VegaFusionError::internal(format!(
@@ -96,4 +50,163 @@ impl TransformTrait for Facet {
 
         Ok((df, output_values))
     }
+}
+
+async fn facet_one_column(
+    dataframe: Arc<dyn DataFrame>,
+    config: &CompilationConfig,
+    group_field: &String,
+    transforms: &[Transform],
+) -> Result<(Arc<dyn DataFrame>, Vec<TaskValue>)> {
+    // Unescape field
+    let unescaped_group_field = unescape_field(&group_field);
+    let group_col = unescaped_col(&group_field);
+
+    // Collect unique values of group column
+    let table = dataframe
+        .aggregate(vec![group_col.clone()], vec![])
+        .await?
+        .collect()
+        .await?;
+    let batch = table.to_record_batch()?;
+    let col_array = batch.column(0);
+    let col_values = (0..col_array.len())
+        .map(|i| Ok(ScalarValue::try_from_array(col_array, i)?))
+        .collect::<Result<Vec<_>>>()?;
+
+    // Collect output schema and batches
+    let mut final_schema = Schema::empty();
+    let mut batches: Vec<RecordBatch> = Vec::new();
+    let mut output_values: Vec<TaskValue> = Vec::new();
+    for col_value in col_values {
+        let filter = if col_value.is_null() {
+            group_col.clone().is_null()
+        } else {
+            group_col.clone().eq(lit(col_value.clone()))
+        };
+
+        let mut filtered_df = dataframe.filter(filter).await?;
+
+        // Apply transform pipeline
+        for tx in transforms {
+            let (tx_df, tx_out_vals) = tx.eval(filtered_df, config).await?;
+            output_values.extend(tx_out_vals);
+            filtered_df = tx_df;
+        }
+
+        // Add group value column back
+        let mut selections = filtered_df
+            .schema()
+            .fields
+            .iter()
+            .filter_map(|f| {
+                if f.name() == &unescaped_group_field {
+                    None
+                } else {
+                    Some(flat_col(f.name()))
+                }
+            })
+            .collect::<Vec<_>>();
+        selections.insert(0, lit(col_value).alias(&unescaped_group_field));
+        filtered_df = filtered_df.select(selections).await?;
+
+        // Grab schema
+        final_schema = filtered_df.schema();
+
+        // evaluate to batches
+        let final_table = filtered_df.collect().await?;
+        batches.extend(final_table.batches);
+    }
+
+    let combined = VegaFusionTable::try_new(Arc::new(final_schema), batches)?;
+    let df = dataframe.connection().scan_arrow(combined).await?;
+    Ok((df, output_values))
+}
+
+async fn facet_two_columns(
+    dataframe: Arc<dyn DataFrame>,
+    config: &CompilationConfig,
+    group_field0: &String,
+    group_field1: &String,
+    transforms: &[Transform],
+) -> Result<(Arc<dyn DataFrame>, Vec<TaskValue>)> {
+    // Unescape fields
+    let unescaped_group_field0 = unescape_field(&group_field0);
+    let group_col0 = unescaped_col(&group_field0);
+
+    let unescaped_group_field1 = unescape_field(&group_field1);
+    let group_col1 = unescaped_col(&group_field1);
+
+    // Collect unique values of group column
+    let table = dataframe
+        .aggregate(vec![group_col0.clone(), group_col1.clone()], vec![])
+        .await?
+        .collect()
+        .await?;
+    let batch = table.to_record_batch()?;
+    let col_array0 = batch.column(0);
+    let col_array1 = batch.column(1);
+
+    let col_values = (0..col_array0.len())
+        .map(|i| {
+            let scalar0 = ScalarValue::try_from_array(col_array0, i)?;
+            let scalar1 = ScalarValue::try_from_array(col_array1, i)?;
+            Ok((scalar0, scalar1))
+        })
+        .collect::<Result<Vec<_>>>()?;
+
+    // Collect output schema and batches
+    let mut final_schema = Schema::empty();
+    let mut batches: Vec<RecordBatch> = Vec::new();
+    let mut output_values: Vec<TaskValue> = Vec::new();
+    for (col0_value, col1_value) in col_values {
+        let filter0 = if col0_value.is_null() {
+            group_col0.clone().is_null()
+        } else {
+            group_col0.clone().eq(lit(col0_value.clone()))
+        };
+        let filter1 = if col1_value.is_null() {
+            group_col1.clone().is_null()
+        } else {
+            group_col1.clone().eq(lit(col1_value.clone()))
+        };
+
+        let mut filtered_df = dataframe.filter(filter0.and(filter1)).await?;
+
+        // Apply transform pipeline
+        for tx in transforms {
+            let (tx_df, tx_out_vals) = tx.eval(filtered_df, config).await?;
+            output_values.extend(tx_out_vals);
+            filtered_df = tx_df;
+        }
+
+        // Add group value column back
+        let mut selections = filtered_df
+            .schema()
+            .fields
+            .iter()
+            .filter_map(|f| {
+                if f.name() == &unescaped_group_field0 || f.name() == &unescaped_group_field1 {
+                    None
+                } else {
+                    Some(flat_col(f.name()))
+                }
+            })
+            .collect::<Vec<_>>();
+        selections.insert(0, lit(col1_value).alias(&unescaped_group_field1));
+        selections.insert(0, lit(col0_value).alias(&unescaped_group_field0));
+        filtered_df = filtered_df.select(selections).await?;
+
+        // Grab schema
+        final_schema = filtered_df.schema();
+
+        // evaluate to batches
+        let final_table = filtered_df.collect().await?;
+        batches.extend(final_table.batches);
+    }
+
+    // Build combined DataFrame
+    let combined = VegaFusionTable::try_new(Arc::new(final_schema), batches)?;
+    let df = dataframe.connection().scan_arrow(combined).await?;
+    Ok((df, output_values))
 }
--- vegafusion-runtime/tests/test_transform_facet.rs
+++ vegafusion-runtime/tests/test_transform_facet.rs
@@ -13,7 +13,7 @@ use vegafusion_core::spec::transform::TransformSpec;
 use vegafusion_runtime::expression::compiler::config::CompilationConfig;
 
 #[test]
-fn test_facet_simple() {
+fn test_facet_1() {
     let dataset = vega_json_dataset("penguins");
 
     let transform_specs: Vec<TransformSpec> = serde_json::from_value(json!([
@@ -36,5 +36,60 @@ fn test_facet_simple() {
     let (result_data, result_signals) =
         eval_vegafusion_transforms(&dataset, transform_specs.as_slice(), &Default::default());
 
-    println!("{}", result_data.pretty_format(None).unwrap())
+    assert_eq!(
+        result_data.pretty_format(None).unwrap(),
+        "\
++--------+------------------+
+| Sex    | count_beak_depth |
++--------+------------------+
+| MALE   | 168              |
+| FEMALE | 165              |
+|        | 10               |
+| .      | 1                |
++--------+------------------+"
+    )
+}
+
+#[test]
+fn test_facet_2() {
+    let dataset = vega_json_dataset("penguins");
+
+    let transform_specs: Vec<TransformSpec> = serde_json::from_value(json!([
+        {
+            "type": "facet",
+            "groupby": ["Sex", "Island"],
+            "transform": [
+                {
+                    "type": "aggregate",
+                    "groupby": [],
+                    "fields": ["Island"],
+                    "op": ["count"],
+                    "as": ["count_beak_depth"]
+                }
+            ]
+        },
+    ]))
+    .unwrap();
+
+    let (result_data, result_signals) =
+        eval_vegafusion_transforms(&dataset, transform_specs.as_slice(), &Default::default());
+
+    assert_eq!(
+        result_data.pretty_format(None).unwrap(),
+        "\
++--------+-----------+------------------+
+| Sex    | Island    | count_beak_depth |
++--------+-----------+------------------+
+| MALE   | Torgersen | 23               |
+| FEMALE | Torgersen | 24               |
+|        | Torgersen | 5                |
+| FEMALE | Biscoe    | 80               |
+| MALE   | Biscoe    | 83               |
+| FEMALE | Dream     | 61               |
+| MALE   | Dream     | 62               |
+|        | Dream     | 1                |
+|        | Biscoe    | 4                |
+| .      | Biscoe    | 1                |
++--------+-----------+------------------+"
+    )
 }
