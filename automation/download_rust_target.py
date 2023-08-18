51b545b04cbdb52f4320b43923a8815a17abcf10
Jon Mease <jonmmease@gmail.com>
2023-08-18 16:25:21 -0400

    Add other java tasks

--- automation/download_rust_target.py
+++ automation/download_rust_target.py
@@ -1,10 +1,11 @@
 import subprocess
+import sys
+
 import requests
 import tempfile
 import os
 import tarfile
 import shutil
-from pathlib import Path
 
 
 def get_rustc_version():
@@ -17,21 +18,21 @@ def get_rustc_version():
     return version_line.split()[1].strip()
 
 
-def ensure_wasm_toolchain():
+def ensure_target(target_name):
     package_root = os.environ.get("PIXI_PACKAGE_ROOT", None)
     if package_root is None:
         raise ValueError("Expected PIXI_PACKAGE_ROOT environment variable to be set")
 
     dest_dir = os.path.join(
-        package_root, ".pixi", "env", "lib", "rustlib", "wasm32-unknown-unknown"
+        package_root, ".pixi", "env", "lib", "rustlib", target_name
     )
     if os.path.exists(dest_dir):
         print("wasm32-unknown-unknown target already installed")
         return
 
     rustc_version = get_rustc_version()
-    url = f"https://static.rust-lang.org/dist/rust-std-{rustc_version}-wasm32-unknown-unknown.tar.gz"
-    print(f"Downloading wasm32-unknown-unknown toolchain from {url}")
+    url = f"https://static.rust-lang.org/dist/rust-std-{rustc_version}-{target_name}.tar.gz"
+    print(f"Downloading {target_name} toolchain from {url}")
     result = requests.get(url)
 
     # Check if the download was successful
@@ -44,7 +45,7 @@ def ensure_wasm_toolchain():
     with tempfile.TemporaryDirectory() as temp_dir:
         # Write the downloaded content to a temporary file
         temp_file_path = os.path.join(
-            temp_dir, f"rust-std-{rustc_version}-wasm32-unknown-unknown.tar.gz"
+            temp_dir, f"rust-std-{rustc_version}-{target_name}.tar.gz"
         )
         with open(temp_file_path, "wb") as temp_file:
             temp_file.write(result.content)
@@ -56,19 +57,19 @@ def ensure_wasm_toolchain():
         # Define the source and destination directories
         src_dir = os.path.join(
             temp_dir,
-            f"rust-std-{rustc_version}-wasm32-unknown-unknown",
-            "rust-std-wasm32-unknown-unknown",
+            f"rust-std-{rustc_version}-{target_name}",
+            f"rust-std-{target_name}",
             "lib",
             "rustlib",
-            "wasm32-unknown-unknown"
+            target_name
         )
 
         # Copy the directory
         if os.path.exists(dest_dir):
             shutil.rmtree(dest_dir)
         shutil.copytree(src_dir, dest_dir)
-        print(f"Installed wasm toolchain to {dest_dir}")
+        print(f"Installed {target_name} toolchain to {dest_dir}")
 
 
 if __name__ == '__main__':
-    ensure_wasm_toolchain()
+    ensure_target(sys.argv[1])
