bb02573ff96eacc1bbf061e90d1bf18908022e86
Jon Mease <jonmmease@gmail.com>
2024-02-19 09:20:10 -0500

    Handle unescaping fields aggregate/fold/pivot

--- vegafusion-core/src/spec/transform/pivot.rs
+++ vegafusion-core/src/spec/transform/pivot.rs
@@ -67,8 +67,9 @@ impl TransformSpecTrait for PivotTransformSpec {
             .unwrap_or_default()
             .iter()
             .filter_map(|groupby_field| {
-                if input_local_datetime_columns.contains(groupby_field) {
-                    Some(groupby_field.clone())
+                let unescaped = unescape_field(&groupby_field);
+                if input_local_datetime_columns.contains(&unescaped) {
+                    Some(unescaped)
                 } else {
                     None
                 }
