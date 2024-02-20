4783a440a9a15c28cb198717a3c4492c7e0bfb1a
Jon Mease <jonmmease@gmail.com>
2024-02-20 14:49:39 -0500

    Revert "Update test"
    This reverts commit d474eb2733332a41ce8d345ae9048bf9efcbdb0b.

--- vegafusion-common/src/data/json_writer.rs
+++ vegafusion-common/src/data/json_writer.rs
@@ -799,8 +799,8 @@ mod tests {
 
         assert_eq!(
             String::from_utf8(buf).unwrap(),
-            r#"{"micros":1542129070011,"millis":1542129070011,"name":"a","nanos":1542129070011,"secs":1542129070000}
-{"micros":null,"millis":null,"name":"b","nanos":null,"secs":null}
+            r#"{"nanos":1542129070011,"micros":1542129070011,"millis":1542129070011,"secs":1542129070000,"name":"a"}
+{"nanos":null,"micros":null,"millis":null,"secs":null,"name":"b"}
 "#
         );
     }
@@ -888,8 +888,8 @@ mod tests {
 
         assert_eq!(
             String::from_utf8(buf).unwrap(),
-            r#"{"name":"a","time32msec":"00:00:00.120","time32sec":"00:02:00","time64nsec":"00:00:00.000000120","time64usec":"00:00:00.000120"}
-{"name":"b","time32msec":null,"time32sec":null,"time64nsec":null,"time64usec":null}
+            r#"{"time32sec":"00:02:00","time32msec":"00:00:00.120","time64usec":"00:00:00.000120","time64nsec":"00:00:00.000000120","name":"a"}
+{"time32sec":null,"time32msec":null,"time64usec":null,"time64nsec":null,"name":"b"}
 "#
         );
     }
@@ -931,8 +931,8 @@ mod tests {
 
         assert_eq!(
             String::from_utf8(buf).unwrap(),
-            r#"{"duration_msec":"PT0.12S","duration_nsec":"PT0.00000012S","duration_sec":"PT120S","duration_usec":"PT0.00012S","name":"a"}
-{"duration_msec":null,"duration_nsec":null,"duration_sec":null,"duration_usec":null,"name":"b"}
+            r#"{"duration_sec":"PT120S","duration_msec":"PT0.120S","duration_usec":"PT0.000120S","duration_nsec":"PT0.000000120S","name":"a"}
+{"duration_sec":null,"duration_msec":null,"duration_usec":null,"duration_nsec":null,"name":"b"}
 "#
         );
     }
