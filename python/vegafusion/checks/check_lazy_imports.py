48f36bba0a29163ffc221e445317b54299f0ffdb
Jon Mease <jonmmease@gmail.com>
2024-05-09 10:11:44 -0400

    Add altair to lazy import checks

--- python/vegafusion/checks/check_lazy_imports.py
+++ python/vegafusion/checks/check_lazy_imports.py
@@ -2,5 +2,5 @@ import sys
 import vegafusion as vf
 import vegafusion_embed
 
-for mod in ["polars", "pandas", "pyarrow", "duckdb"]:
-    assert mod not in sys.modules
+for mod in ["polars", "pandas", "pyarrow", "duckdb", "altair"]:
+    assert mod not in sys.modules, f"{mod} module should be imported lazily"
