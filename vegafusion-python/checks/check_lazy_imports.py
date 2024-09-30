20eadd25270ce1a7ebf6ea0a1a3075b3741c40c7
Jon Mease <jonmmease@gmail.com>
2024-09-30 13:43:55 -0400

    v2: Combine Python packages (#511)
    * combine vegafusion and vegafusion-python-embed
    
    * fix exclude
    
    * action fixes
    
    * exclude based on crate name not directory name
    
    * Fix pip install paths
    
    * don't try to import embed

--- vegafusion-python/checks/check_lazy_imports.py
+++ vegafusion-python/checks/check_lazy_imports.py
@@ -1,6 +1,5 @@
 import sys
 import vegafusion as vf
-import vegafusion_embed
 
 for mod in ["polars", "pandas", "pyarrow", "duckdb", "altair"]:
     assert mod not in sys.modules, f"{mod} module should be imported lazily"
